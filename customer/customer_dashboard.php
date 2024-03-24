d60dc9ad5e9631e83fb0bde6580626337ad63ec8
Gino <kirigayakazuto03@rocketmail.com>
2024-03-25 02:37:45 +0800

    another revision

--- customer/customer_dashboard.php
+++ customer/customer_dashboard.php
@@ -101,42 +101,38 @@ if (!isset ($_SESSION['User'])) {
             </div>
         </nav>
         <div class="container-fluid py-4">
+            POULTRIES
             <div class="row">
-                <div class="col-lg-6 position-relative z-index-2">
-                    <div class="row">
-                        <div class="col-lg-6 col-sm-3">
-                            <div class="card  mb-2">
-                                <div class="card-header p-3 pt-2">
-                                    <div
-                                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
-                                        <i class="fa-solid fa-egg"></i>
-                                    </div>
-                                    <div class="text-end pt-1">
-                                        <p class="text-sm mb-0 text-capitalize">Total Egg Sold</p>
-                                        <h4 class="mb-0" id="totalEggSold">0</h4>
+                <div class="col-lg-2">
+                    <div class="card card-plain mb-4">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="d-flex flex-column h-100">
                                     </div>
                                 </div>
-                                <hr class="dark horizontal my-0">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-3">
-                            <div class="card  mb-2">
-                                <div class="card-header p-3 pt-2">
-                                    <div
-                                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
-                                        <i class="fa-solid fa-coins"></i>
-                                    </div>
-                                    <div class="text-end pt-1">
-                                        <p class="text-sm mb-0 text-capitalize">Total Sales</p>
-                                        <h4 class="mb-0" id="totalSalesCounter">0</h4>
-                                    </div>
-                                </div>
-                                <hr class="dark horizontal my-0">
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card card-plain mb-4">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="d-flex flex-column h-100">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 position-relative z-index-2">
                     <button class="btn btn-primary" style="float:right;margin-top:2%; margin-left: 2%;"
-                        data-bs-toggle="modal" data-bs-target="#addSales">Add Sales</button>
+                        data-bs-toggle="modal" data-bs-target="#addSales"><i class="fa-solid fa-cart-plus"></i>
+                        Order</button>
                     <br>
                     <br>
                     <br>
@@ -167,70 +163,13 @@ if (!isset ($_SESSION['User'])) {
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card mb-4 ">
-                                <div class="d-flex">
-                                    <div
-                                        class="icon icon-shape icon-lg bg-gradient-success shadow text-center border-radius-xl mt-n3 ms-4">
-                                        <i class="fa-solid fa-chart-line"></i>
-                                    </div>
-                                    <h6 class="mt-3 mb-2 ms-3 ">Sales Chart</h6>
-                                </div>
-                                <div class="card-body p-3">
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-7">
-                                            <canvas id="mychart"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div id="globe" class="position-absolute end-0 top-10 mt-sm-3 mt-7 me-lg-7">
-                    <canvas width="700" height="600"
-                        class="w-lg-100 h-lg-100 w-75 h-75 me-lg-0 me-n10 mt-lg-5"></canvas>
+
                 </div>
             </div>
         </div>
     </main>
 
-    <!--=====THIS MODAL IS FOR ADDING SALES=====-->
-    <div class="modal fade" id="addSales" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Add Sales</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <h6>Date</h6>
-                        <input type="date" class="form-control addSalesDate" id="sdate">
-                    </div>
-                    <div class="form-group">
-                        <h6>Quantity of Eggs</h6>
-                        <input type="number" class="form-control" id="sqty">
-                    </div>
-                    <div class="form-group">
-                        <h6>Total Sales</h6>
-                        <input type="number" class="form-control" id="stotal">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" id="saveSales" class="btn btn-primary">ADD</button>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <script src="assets/js/material-dashboard.min.js?v=3.1.0"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"
